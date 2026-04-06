0562da8b3dddf970542adebca87a9d16c30f0a55
Resul Özyurt <rozyurt791@gmail.com>
2026-04-07 00:21:52 +0300

    backend bugfix v3

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000/api/v1',
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://marketing-sales-hub-production.up.railway.app/api/v1',
 });
 
 // Request Interceptor: Automatically attach the JWT token to every request
